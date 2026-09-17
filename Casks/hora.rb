2e0363f6ec59c033a23f26f344b382892e4a5d19
Maciej Szamowski <maciek.szamowski@gmail.com>
2026-09-17 10:45:42 +0200

    chore: update hora to 1.1.0 (482)

--- Casks/hora.rb
+++ Casks/hora.rb
@@ -1,8 +1,8 @@
 cask "hora" do
-  version "1.0.34,457"
-  sha256 "dac94c320cf25ceef1878d705983e92a5752e69d2a3a7d5215000d367f63289a"
+  version "1.1.0,482"
+  sha256 "e00d03985c650d994c3fd57881cc3d5b379cc5d8dd95f6ddafc1c09d4d6342c9"
 
-  url "https://downloads.horacal.app/direct/stable/releases/1.0.34/457/hora-calendar-1.0.34-457.zip"
+  url "https://downloads.horacal.app/direct/stable/releases/1.1.0/482/hora-calendar-1.1.0-482.zip"
   name "hora Calendar"
   desc "Calendar app for focused planning"
   homepage "https://horacal.app"
